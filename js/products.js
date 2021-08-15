c88bd40afebdf22da1764a8f71bd2e43bb4898f0
martin vora <martin.vora@gmail.com>
2021-08-15 11:40:33 -0300

    Petición Json y productos agregados

--- js/products.js
+++ js/products.js
@@ -1,6 +1,44 @@
+var productsArray = [];
+
+function showProductsList(array){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i <   array.length; i++){
+        let products = array[i];
+
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + products.imgSrc + `" alt="` + products.description + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ products.name +`</h4>
+                        <small class="text-muted">` + products.cost + ` U$S </small>
+                        
+                    </div>
+                    <smaller h4 class="mb-1">` + products.description + `</h4>
+                </div>
+            </div>
+        </div>
+        `
+
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+    }
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
-
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCTS_URL ).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            productsArray = resultObj.data;
+            //Muestro los productos ordenados
+            showProductsList(productsArray);
+        }
+        
+    });
 });

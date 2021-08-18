bef2dd147b366fa1509e9b1b75e1333aee451736
martin vora <martin.vora@gmail.com>
2021-08-17 22:52:20 -0300

    function conectado y boton de deslogueo

---
+++ js/conectado.js
@@ -0,0 +1,12 @@
+
+function conectado() {
+let usuario = JSON.parse(localStorage.getItem("usuario"));
+let nombre_usuario = document.getElementById('nombre');
+let botondeslogueo = document.getElementById('btnlogin');
+
+nombre_usuario.innerHTML= "Hola"+" "+usuario.nombre+"!!";
+nombre_usuario.style.color ="white";
+botondeslogueo.classList.remove("invisible");
+
+
+};

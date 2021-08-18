fa6afcde4d936ad3c651ecce5eb06a675104f803
martin vora <martin.vora@gmail.com>
2021-08-17 22:53:40 -0300

    armado de login con funcion verificar y desconectar

--- js/login.js
+++ js/login.js
@@ -1,6 +1,49 @@
+
+
+function verificar() {
+    let dato = document.getElementById('user');
+    let alerta = document.getElementById('alerta');
+    let password = document.getElementById('pass');
+    let usuario = {};
+    
+    
+    if (dato.value.trim() === '' || password.value.trim() === '') {
+        
+        dato.classList.add("isInvalid");
+        alerta.innerHTML = "Complete los campos vacíos";
+        alerta.style.color = "red";
+        alerta.style.display = "block";
+       
+    }
+    else {
+        location.href = "index.html";
+        usuario.nombre = dato.value;
+        usuario.estado = "conectado";
+        localStorage.setItem('usuario', JSON.stringify(usuario));
+        
+        
+    
+
+    }
+}
+
+
+
+
+function desconectar(){
+    localStorage.clear();
+    location.href="index.html";
+    }
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
+    
 
+        conectado();    
+
+  
+    
 });

b8d2c9f44b3bb39a3f6d9a49b48baec4893241a5
martin vora <martin.vora@gmail.com>
2021-08-19 19:10:05 -0300

    conectado

--- js/conectado.js
+++ js/conectado.js
@@ -1,10 +1,11 @@
+// saluda al usuario y hace visibile el boton de desconectar 
 
 function conectado() {
 let usuario = JSON.parse(localStorage.getItem("usuario"));
 let nombre_usuario = document.getElementById('nombre');
 let botondeslogueo = document.getElementById('btnlogin');
 
-nombre_usuario.innerHTML= "Hola"+" "+usuario.nombre+"!!";
+nombre_usuario.innerHTML= "Hola"+" "+usuario.nombre+"!";
 nombre_usuario.style.color ="white";
 botondeslogueo.classList.remove("invisible");
 

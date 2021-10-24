1b4e04d85b28a5ca2d79336221cbe4787ea3b6c6
martin vora <martin.vora@gmail.com>
2021-10-24 16:47:26 -0400

    entrega 6

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,40 +1,53 @@
+    
     function editarPerfil () {
         
                 
-
+            let perfil = {};
             let datoNombre = document.getElementById('editarNombres');
             let datoApellido = document.getElementById('editarApellidos');
-            let perfil = {};
+            let datoEdad = document.getElementById('editarEdad');
+            let datoEmail = document.getElementById('editarEmail');
+            let datoTelefono = document.getElementById('editarTelefono');
             
-            
-           
-            
-                perfil.nombres = datoNombre.value;
-                perfil.apellidos = datoApellido.value;
-              
-                localStorage.setItem('perfil', JSON.stringify(perfil));
-                location.reload();
-                
-                
-            
-        
-            
-        
     
+            perfil.nombres = datoNombre.value;
+            perfil.apellidos = datoApellido.value;
+            perfil.edad = datoEdad.value;  
+            perfil.email = datoEmail.value;
+            perfil.telefono = datoTelefono.value;
+            
+            
+            localStorage.setItem('perfil', JSON.stringify(perfil));
+            location.reload();
+
+
+
         };
     
        
     function mostrarPerfil () {
 
         
-        let perfilDatos1 = JSON.parse(localStorage.getItem('perfil'));
+        let perfilDatos = JSON.parse(localStorage.getItem('perfil'));
+            
             let mostrarNombre = document.getElementById("nombrePerfil");
             let mostrarApellido = document.getElementById("apellidoPerfil");
-
-            mostrarNombre.innerHTML = perfilDatos1.nombres 
-            mostrarApellido.innerHTML = perfilDatos1.apellidos
+            let mostraEdad = document.getElementById("edadPerfil");
+            let mostrarEmail = document.getElementById("emailPerfil");
+            let mostrarTelefono = document.getElementById("telefonoPerfil");
             
-    }            
+
+            mostrarNombre.innerHTML = perfilDatos.nombres 
+            mostrarApellido.innerHTML = perfilDatos.apellidos
+            mostraEdad.innerHTML = perfilDatos.edad
+            mostrarEmail.innerHTML = perfilDatos.email
+            mostrarTelefono.innerHTML = perfilDatos.telefono
+            
+            
+            
+            
+    } ;           
+
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
--- js/products.js
+++ js/products.js
@@ -25,21 +25,18 @@ function verificacion() {
         let products = lista[i];
 
         htmlContentToAppend += `
-        <div class="list-group-item list-group-item-action">
-            <div class="row">
-                <div class="col-3">
-                    <img src="` + products.imgSrc + `" alt="` + products.description + `" class="img-thumbnail">
-                </div>
-                <div class="col">
-                    <div class="d-flex w-100 justify-content-between">
-                        <h4 class="mb-1">`+ products.name + ` U$S `+products.cost+ ` </h4>
-                        <small class="text-muted">` + products.soldCount + ` Vendidos </small>
-                        
-                    </div>
-                    <smaller h4 class="mb-1">` + products.description + `</h4>
-                </div>
+        
+        <a href="product-info.html" class="card mb-4 shadow-sm custom-car ">
+            
+           <img src="` + products.imgSrc + `" alt="` + products.description + `" class="bd-placeholder-img card-img-top">                 
+           <h4 class="m-3">`+ products.name + ` U$S `+products.cost+ ` </h4>
+         
+           <div class="card-body">
+             <smaller h4 class="card-text">` + products.description + `</h4> <br>
+             <small class="text-muted">` + products.soldCount + ` Vendidos </small>
             </div>
-        </div>
+        </a>
+        
         `
 
         document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
@@ -86,21 +83,15 @@ function showProductsList(){
             ((maxCount == undefined) || (maxCount != undefined && parseInt(products.cost) <= maxCount))){
 
         htmlContentToAppend += `
-        <a href="product-info.html" class="list-group-item list-group-item-action">
-                <div class="row">
-                    <div class="col-3">
-                        <img src="` + products.imgSrc + `" alt="` + products.description + `" class="img-thumbnail">
-                    </div>
-                    <div class="col">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">`+ products.name + ` U$S `+products.cost+ ` </h4>
-                            <small class="text-muted">` + products.soldCount + ` Vendidos </small>
+        <a href="product-info.html" class="card mb-4 shadow-sm custom-car ">
             
-                        </div>
-                        <smaller h4 class="mb-1">` + products.description + `</h4>
-                    </div>
-                </div>
-           
+           <img src="` + products.imgSrc + `" alt="` + products.description + `" class="bd-placeholder-img card-img-top">                 
+           <h4 class="m-3">`+ products.name + ` U$S `+products.cost+ ` </h4>
+         
+           <div class="card-body">
+             <smaller h4 class="card-text">` + products.description + `</h4> <br>
+             <small class="text-muted">` + products.soldCount + ` Vendidos </small>
+            </div>
         </a>
         `
             }
